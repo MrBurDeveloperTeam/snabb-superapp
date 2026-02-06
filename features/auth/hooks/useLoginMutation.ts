4c3fb0ac2f7284069566763964e559dfbdaa7caa
Nizam Yuseri <leadexpert@mr-bur.com>
2026-02-06 14:45:11 +0800

    add log in login mutation

--- features/auth/hooks/useLoginMutation.ts
+++ features/auth/hooks/useLoginMutation.ts
@@ -7,6 +7,7 @@ export const useLoginMutation = (onAuthSuccess: () => void) => {
   return useMutation({
     mutationFn: async (data: LoginFormInputs) => {
       const loginResult = await loginOdoo(data.login, data.password);
+      console.log('loginResult:', loginResult);
       return { sessionInfo: loginResult.data.result };
     },
     onSuccess: ({ sessionInfo }) => {
