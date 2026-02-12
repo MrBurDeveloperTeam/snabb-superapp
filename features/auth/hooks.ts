229aa5770cd283fc26ef0a1dfc6a0f44791a1923
Nizam Yuseri <leadexpert@mr-bur.com>
2026-02-12 10:15:50 +0800

    cleanup login hooks

--- features/auth/hooks.ts
+++ features/auth/hooks.ts
@@ -1,18 +1,18 @@
-import { loginOdoo } from "@/services/LoginOdoo";
-import { useMutation } from "@tanstack/react-query";
-import { LoginFormInputs } from "./types/LoginPageProps";
-import { getSessionInfo } from "@/services/GetSessionInfo";
+// import { loginOdoo } from "@/services/LoginOdoo";
+// import { useMutation } from "@tanstack/react-query";
+// import { LoginFormInputs } from "./types/LoginPageProps";
+// import { getSessionInfo } from "@/services/GetSessionInfo";
 
-export const loginMutation = useMutation({
-    mutationFn: async (data: LoginFormInputs) => {
-      const loginResult = await loginOdoo(data.login, data.password);
-      const sessionInfo = await getSessionInfo(data.login, data.password, loginResult.data);
-      return { sessionInfo }; 
-    },
-    onSuccess: ({ sessionInfo }) => {
-      localStorage.setItem('odoo_session', JSON.stringify(sessionInfo));
-    },
-    onError: (err: any) => {
-      console.error("Login failed:", err.message);
-    },
-  });
+// export const loginMutation = useMutation({
+//     mutationFn: async (data: LoginFormInputs) => {
+//       const loginResult = await loginOdoo(data.login, data.password);
+//       const sessionInfo = await getSessionInfo(data.login, data.password, loginResult.data);
+//       return { sessionInfo }; 
+//     },
+//     onSuccess: ({ sessionInfo }) => {
+//       localStorage.setItem('odoo_session', JSON.stringify(sessionInfo));
+//     },
+//     onError: (err: any) => {
+//       console.error("Login failed:", err.message);
+//     },
+//   });
