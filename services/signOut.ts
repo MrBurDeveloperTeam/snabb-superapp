71f6fc9990825cf2a7e6ff0205cc690c5826cfba
Nizam Yuseri <leadexpert@mr-bur.com>
2026-03-04 11:55:18 +0800

    fix localstorage clear data

--- services/signOut.ts
+++ services/signOut.ts
@@ -3,6 +3,5 @@ import api from "./api";
 export const signOut = async () => {
   api.post('/logout', {});
   localStorage.removeItem("odoo_session");
-  localStorage.clear();
   sessionStorage.clear();
 };
