de01bf60cd4028e9859f9e04fab8b7d097227283
Nizam Yuseri <leadexpert@mr-bur.com>
2026-03-16 14:11:53 +0800

    fix inventory icon

--- constants.ts
+++ constants.ts
@@ -44,7 +44,7 @@ export const MINI_APPS: MiniApp[] = [
     id: 'app-2',
     title: 'Inventory',
     category: 'Productivity',
-    icon: 'fa-solid fa-boxes-stacked',
+    icon: 'https://mrbur.odoo.com/stock/static/description/icon.png',
     route: `https://inventory.snabbb.com/`,
     colorScheme: { bg: 'bg-[#fef2f2]', text: 'text-[#b91c1c]', icon: '#b91c1c' }
   },
