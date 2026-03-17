57f3f005fbe9a9eac0b50d7c9ab40b32f6808dd1
Nizam Yuseri <leadexpert@mr-bur.com>
2026-03-17 09:10:59 +0800

    fix icon color

--- constants.ts
+++ constants.ts
@@ -44,7 +44,7 @@ export const MINI_APPS: MiniApp[] = [
     id: 'app-2',
     title: 'Inventory',
     category: 'Productivity',
-    icon: 'https://mrbur.odoo.com/stock/static/description/inventory.png',
+    icon: 'https://mrbur.odoo.com/stock/static/description/icon.png',
     route: `https://inventory.snabbb.com/`,
     colorScheme: { bg: 'bg-[#fef2f2]', text: 'text-[#b91c1c]', icon: '#b91c1c' }
   },
