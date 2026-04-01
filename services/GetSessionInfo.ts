b0c3c2d9c6e0a31ea44d36c12393a6ddbe424f69
Nizam Yuseri <leadexpert@mr-bur.com>
2026-04-01 11:42:47 +0800

    fixed signup redirect

--- services/GetSessionInfo.ts
+++ services/GetSessionInfo.ts
@@ -3,6 +3,9 @@ import api from "./api";
 export const getSessionInfo = async () => {
   const response = await api.post('/web/session/get_session_info', {});
   console.log("Session Info Response: ", response);
+  if(response.data.error) {
+    throw new Error(response.data.error.message);
+  }
   const data = await response.data;
   return data.result;
 };
