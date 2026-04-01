45ed93d5d721e31940ffc73d3a286c823f27b0dc
Nizam Yuseri <leadexpert@mr-bur.com>
2026-04-01 12:02:25 +0800

    fixed signup redirect

--- App.tsx
+++ App.tsx
@@ -91,6 +91,7 @@ const App: React.FC = () => {
   const verifySession = useCallback(async () => {
     try {
       const res = (await getSessionInfo()) as any;
+      console.log('the res up: ',res);
       console.log('the res: ',!res?.sessionInfo);
       if (!res?.sessionInfo) {
         clearAuthState();
