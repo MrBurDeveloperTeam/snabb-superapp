f03269f9a7ed47b073ffa5cd687dc40d121c17b2
Nizam Yuseri <leadexpert@mr-bur.com>
2026-04-16 21:35:19 +0800

    fix ui blocking when error thrown

--- features/auth/hooks/useGetSessionInfo.ts
+++ features/auth/hooks/useGetSessionInfo.ts
@@ -8,13 +8,13 @@ const useGetSessionInfo = () => {
             // Make sure getSessionInfo returns a valid response.
             const response = await getSessionInfo();
             if (!response) {
-                throw new Error("No session info found");
+                return new Error("No session info found");
             }
 
             // If the response is valid, try to get sessionInfo with retry
             const sessionInfo = await getSessionInfoWithRetry();
             if (!sessionInfo) {
-                throw new Error("Session info with retry failed");
+                return new Error("Session info with retry failed");
             }
 
             // Ensure sessionInfo exists before proceeding
@@ -25,7 +25,7 @@ const useGetSessionInfo = () => {
         } catch (error) {
             console.error("Error fetching session info:", error);
             // Handle error (e.g., show user-friendly message or fallback)
-            throw error; // rethrow or handle as needed
+            return error; // rethrow or handle as needed
         }
     },
     onSuccess: (data) => {
