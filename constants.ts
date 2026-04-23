5944b2e792c0bbb375b19f318fd2d0f18e5013ef
Nizam Yuseri <leadexpert@mr-bur.com>
2026-04-23 15:39:01 +0800

    re-enable elearning

--- constants.ts
+++ constants.ts
@@ -101,7 +101,7 @@ export const MINI_APPS: MiniApp[] = [
     title: 'E-Learning',
     category: 'Productivity',
     icon: 'https://app.snabbb.com/icons/e-learning.png',
-    // route: `https://app.snabbb.com`,
+    route: `https://e-learning.snabbb.com/`,
     colorScheme: { bg: 'bg-[#F9FBFA]', text: 'text-[#b45309]', icon: '#0891b2' }
   },
   {
