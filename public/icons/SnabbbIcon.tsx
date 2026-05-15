938290b9b18e2673afd4b9d0822789aad032de81
Nizam Yuseri <leadexpert@mr-bur.com>
2026-05-15 15:02:48 +0800

    fix snabbb icon

--- public/icons/SnabbbIcon.tsx
+++ public/icons/SnabbbIcon.tsx
@@ -2,6 +2,6 @@ import Snabbb from './Snabbb-Teal.png';
 
 export const SnabbbIcon = () => {
     return (
-        <img src={Snabbb} alt="Snabbb Icon" className="w-5 h-5 sm:w-7 sm:h-7" />
+        <img src={Snabbb} alt="Snabbb Icon" />
     );
 }
