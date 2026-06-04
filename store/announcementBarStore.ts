d54507ea19330722f5fa440793025681c7ce387f
Nizam Yuseri <leadexpert@mr-bur.com>
2026-06-04 15:51:47 +0800

    change url for account

--- store/announcementBarStore.ts
+++ store/announcementBarStore.ts
@@ -45,7 +45,7 @@ const DEFAULT_CONFIG: AnnouncementBarConfig = {
   targetIncomplete: true,
   textIncomplete:
     "Please fill up your profile to get a extra coupon from us. Click Here Edit.",
-  linkIncomplete: "https://mrbur.shop/my/account",
+  linkIncomplete: "https://account.snabbb.com",
 };
 
 export const useAnnouncementBarStore = create<AnnouncementBarStore>()(
