573efdb7123b7ae6e5fc129bf496e05e12ad4446
Nizam Yuseri <leadexpert@mr-bur.com>
2026-06-11 16:01:55 +0800

    fix login mutation

--- features/auth/hooks/useLoginMutation.ts
+++ features/auth/hooks/useLoginMutation.ts
@@ -6,7 +6,8 @@ export const useLoginMutation = (onAuthSuccess: () => void) => {
   return useMutation({
     mutationFn: async (data: AuthFormInputs) => {
       const loginResult = await loginOdoo(data.login, data.password);
-      console.log("loginResult:", loginResult); 
+      console.log("loginResult keys:", Object.keys(loginResult));
+      console.log("seed_entry_url:", loginResult.seed_entry_url);
       return {
         sessionInfo: loginResult.data.result,
         seed_entry_url: loginResult.seed_entry_url ?? null,
