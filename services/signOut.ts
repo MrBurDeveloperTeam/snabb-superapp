7386144e33c8224425b25f21fc7528be7c02b8fe
Nizam Yuseri <leadexpert@mr-bur.com>
2026-07-07 09:17:11 +0800

    dont clear all pet stats in local storage

--- services/signOut.ts
+++ services/signOut.ts
@@ -2,6 +2,7 @@ import api from "./api";
 
 export const signOut = async () => {
   await api.post('/logout', {});
-  localStorage.clear();
+  localStorage.removeItem("pet_stats");
+  localStorage.removeItem("odoo_session");
   sessionStorage.clear();
 };
