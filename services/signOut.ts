d2e1a7c03d41594012a12a9d4986a1da406c6e27
Nizam Yuseri <leadexpert@mr-bur.com>
2026-07-07 14:02:02 +0800

    Revert "dont clear all pet stats in local storage"
    This reverts commit 7386144e33c8224425b25f21fc7528be7c02b8fe.

--- services/signOut.ts
+++ services/signOut.ts
@@ -2,7 +2,6 @@ import api from "./api";
 
 export const signOut = async () => {
   await api.post('/logout', {});
-  localStorage.removeItem("pet_stats");
-  localStorage.removeItem("odoo_session");
+  localStorage.clear();
   sessionStorage.clear();
 };
