14e2678f89714a6056a3a99f6012c340a4ea7d5b
Nizam Yuseri <leadexpert@mr-bur.com>
2026-07-15 09:35:54 +0800

    change to double click for cat to move

--- components/CatMascot.tsx
+++ components/CatMascot.tsx
@@ -655,9 +655,9 @@ export default function CatMascot({ onCatClick, disabled = false, isHidden = fal
       }, duration * 1000);
     };
 
-    document.addEventListener('click', handleGlobalClick);
+    document.addEventListener('dblclick', handleGlobalClick);
     return () => {
-      document.removeEventListener('click', handleGlobalClick);
+      document.removeEventListener('dblclick', handleGlobalClick);
       if (autoCloseTimerRef.current) clearTimeout(autoCloseTimerRef.current);
     };
   }, []);
