b5450f4a69d754b319b43e83812f6c0412d27d10
n1cole111 <nihung43@gmail.com>
2026-09-22 13:40:22 +0800

    add charting workspace

--- components/AppCard.tsx
+++ components/AppCard.tsx
@@ -112,8 +112,8 @@ const AppCard: React.FC<AppCardProps> = ({ app, index, isLoggedIn, onOpenEmbedde
         let targetUrl = res.result?.url;
 
         if (
-          (appCode === "inventory" || appCode === "appointment") &&
-            targetUrl
+           ["inventory", "appointment", "charting"].includes(appCode) &&
+           targetUrl
         ) {
           const selectedCompanyOwnerId =
             localStorage.getItem(
